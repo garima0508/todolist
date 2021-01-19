fead1ed6a3e509fc8f368cf970526a6fa80fc1eb
garima0508 <gammo0508@gmail.com>
2021-01-20 00:07:58 +0530

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import TodoListApp from './TodoListApp';
-import './TodoList.css';
-ReactDOM.render(<TodoListApp/>,document.getElementById('root'));
-
